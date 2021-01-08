4e0b3f992d9c931b6480d73234de70934216c4ca
Shubham Shrimant <32372277+shubhamshrimant@users.noreply.github.com>
2021-01-08 18:17:36 +0530

    Update polyreg.py

--- PR/polyreg.py
+++ PR/polyreg.py
@@ -5,7 +5,7 @@ Created on Thu Jan  7 18:42:10 2021
 @author: shubh
 """
 
-# Simple Linear Regression
+# Polynomial Regression
 
 # Importing the libraries
 import numpy as np
@@ -19,14 +19,10 @@ dataset.dropna(inplace=True)
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
 from sklearn.linear_model import LinearRegression
 
-# Training the Simple Linear Regression model on the Training set
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
